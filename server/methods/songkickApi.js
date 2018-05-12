a063f9db2873d66926916cdfa2810f14ef500578
Max McGee <max@Maxs-MacBook-Pro.local>
2018-05-12 15:54:15 -0700

    updates meteor

--- server/methods/songkickApi.js
+++ server/methods/songkickApi.js
@@ -1,49 +1,56 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 
 if (Meteor.isServer) {
+  Meteor.methods({
+    fetchArtistId(artist) {
+      Future = Npm.require("fibers/future");
+      var myFuture = new Future();
 
-	Meteor.methods({
+      HTTP.call(
+        "GET",
+        "http://api.songkick.com/api/3.0/search/artists.json?",
+        {
+          params: {
+            query: artist,
+            apikey: Meteor.settings.songkickApi
+          }
+        },
+        function(error, response) {
+          if (error) {
+            myFuture.return(error);
+          } else {
+            // console.log('artist ID', response);
+            myFuture.return(response);
+          }
+        }
+      );
 
-		fetchArtistId(artist) {
-			Future = Npm.require('fibers/future');
-			var myFuture = new Future();
+      return myFuture.wait();
+    },
 
-			HTTP.call('GET', 'http://api.songkick.com/api/3.0/search/artists.json?', {
-					params: {
-						'query': artist,
-						'apikey': Meteor.settings.songkickApi
-					}
-				}, function (error, response) {
-					if (error) {
-						myFuture.return(error);
-					}	else {
-						// console.log('artist ID', response);
-						myFuture.return(response);
-					}
-			});
+    fetchArtistCalendar(id) {
+      Future = Npm.require("fibers/future");
+      var myFuture = new Future();
 
-			return myFuture.wait();
-		},
+      HTTP.call(
+        "GET",
+        "http://api.songkick.com/api/3.0/artists/" + id + "/calendar.json",
+        {
+          params: {
+            apikey: Meteor.settings.songkickApi
+          }
+        },
+        function(error, response) {
+          if (error) {
+            myFuture.return(error);
+          } else {
+            // console.log('artist Calendar', response);
+            myFuture.return(response);
+          }
+        }
+      );
 
-		fetchArtistCalendar(id) {
-			Future = Npm.require('fibers/future');
-			var myFuture = new Future();
-
-			HTTP.call('GET', 'http://api.songkick.com/api/3.0/artists/' + id + '/calendar.json', {
-				params: {
-					'apikey': Meteor.settings.songkickApi
-				}
-				}, function (error, response) {
-					if (error) {
-						myFuture.return(error);
-					}	else {
-						// console.log('artist Calendar', response);
-						myFuture.return(response);
-					}
-			});
-
-			return myFuture.wait();
-		}
-
-	});
+      return myFuture.wait();
+    }
+  });
 }
